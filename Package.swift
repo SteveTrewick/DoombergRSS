a5dcb85744cac01ad4da497923b073d273e510a5
Steve Trewick <steve.trewick@icloud.com>
2026-01-02 22:05:43 +0000

    Bump DoomLogs to 0.1.0

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/SteveTrewick/DoomLogs.git", from: "0.0.1")
+        .package(url: "https://github.com/SteveTrewick/DoomLogs.git", from: "0.1.0")
     ],
     targets: [
         .target(
